6b16ef52a4d607c3229ff21f696e7cf0e386e91f
YayangSB <yayang.setia.tif420@polban.ac.id>
2024-04-24 11:07:26 +0700

    modify connection between server and client

--- Server/antrianProses.py
+++ Server/antrianProses.py
@@ -1,19 +1,5 @@
 #import library yang dibutuhkan
 import pika
 
-class RegistrationClient:
-    def __init__(self):
-        #Inisialisasi Koneksi RabbitMQ menggunakan koneksi blocking dari library Pika
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-
-        #Pembuatan Channel yang nantinya digunakan untuk melakukan operasi seperti mendeklarasikan antrian dan mengirim pesan
-        self.channel = self.connection.channel()
-
-        #Mendeklarasikan Antrian
-        result = self.channel.queue_declare('', exclusive=True)
-
-        #Menyimpan nama antrian yang telah dibuat ke dalam variabel self.callback_queue
-        self.callback_queue = result.method.queue
-
-        #Mengatur Konsumsi Pesan dari Antrian 
-        self.channel.basic_consume(queue=self.callback_queue, on_message_callback=self.on_response, auto_ack=True)
+#Membuat koneksi antara  Python Anda dengan server RabbitMQ. 
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
